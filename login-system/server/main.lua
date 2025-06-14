9e6f23971acc86a987b015d7356a7ad12db0b24d
Tony <12673334+Tonyx97@users.noreply.github.com>
2025-06-14 11:18:51 +0200

    Removed ACL.

--- login-system/server/main.lua
+++ login-system/server/main.lua
@@ -1,7 +1,6 @@
 local TableResult = SQL.Execute([[
     CREATE TABLE IF NOT EXISTS accounts (
-		steam_id TEXT PRIMARY KEY NOT NULL,
-		acl_level INTEGER NOT NULL
+		steam_id TEXT PRIMARY KEY NOT NULL
     )
 ]])
 
@@ -11,33 +10,20 @@ else
     print("Accounts table creation failed:", TableResult.error)
 end
 
-local acl =
-{
-	["76561198149892734"] = { level = AclLevel.Owner } -- tony
-}
-
 function LoadPlayer(client)
 	if not TableResult.success then return end
 	
 	-- register player if it doesn't exist
 	
 	local steamId = client:GetSteamId()
-	local aclEntry = acl[steamId]
-	local aclLevel = AclLevel.Normal
-
-	-- if player is in the ACL, set its level
 	
-	if aclEntry then aclLevel = aclEntry.level end
-	
-	SQL.Execute("INSERT INTO accounts (steam_id, acl_level) VALUES (?, ?)", steamId, aclLevel)
+	SQL.Execute("INSERT INTO accounts (steam_id) VALUES (?)", steamId)
 	
 	-- get this player's info
 	
-	local entries = SQL.Query("SELECT steam_id, acl_level FROM accounts WHERE steam_id = ?", steamId)
+	local entries = SQL.Query("SELECT steam_id FROM accounts WHERE steam_id = ?", steamId)
 	if #entries == 1 then
-		client:SetAclLevel(entries[1].acl_level)
-		
-		print("Player " .. steamId .. " logged in, acl level: " .. entries[1].acl_level)
+		print("Player " .. steamId .. " logged in: ")
 	else
 		print("Player " .. steamId .." is not allowed to register")
 	end
@@ -45,7 +31,9 @@ end
 
 -- temp shit for now, we don't have ClientResourceStart yet
 
-Event.Add("ResourceStart", function()
+Event.Add("ResourceStart", function(name)
+	if Resource.Name ~= name then return end
+	
 	for k, v in pairs(Players.GetJoined()) do
 		LoadPlayer(v)
 	end
