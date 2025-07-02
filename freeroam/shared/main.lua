92bd5e99beffb7965550eb5b63792478ecabef59
Tony <12673334+Tonyx97@users.noreply.github.com>
2025-07-02 09:35:28 +0200

    Update main.lua

--- freeroam/shared/main.lua
+++ freeroam/shared/main.lua
@@ -1,3 +1,7 @@
 Event.Add("PlayerKilled", function(player)
     print("player " .. player:GetNetId() .. " has been killed.")
 end)
+
+Event.Add("PlayerDamage", function(player, loss)
+    print("player " .. player:GetNetId() .. " damaged " .. loss .. " hp.")
+end)
