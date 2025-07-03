e584f1c54b03a6a848021b456329865af44331e5
Tony <12673334+Tonyx97@users.noreply.github.com>
2025-07-03 19:09:05 +0200

    Added PlayerDamage example parameters.

--- freeroam/shared/main.lua
+++ freeroam/shared/main.lua
@@ -2,6 +2,6 @@ Event.Add("PlayerKilled", function(player)
     print("player " .. player:GetNetId() .. " has been killed.")
 end)
 
-Event.Add("PlayerDamage", function(player, loss)
-    print("player " .. player:GetNetId() .. " damaged " .. loss .. " hp.")
+Event.Add("PlayerDamage", function(player, damager, loss, hitBone, weaponHash, position)
+    print(damager:GetNetId() .. " damaged player " .. player:GetNetId() .. " with " .. loss .. " hp using weapon " .. weaponHash .. " at " .. tostring(position) .. " (bone: " .. hitBone .. ")")
 end)
