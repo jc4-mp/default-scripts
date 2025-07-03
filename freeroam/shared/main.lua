f4bae7ca338ceae5f97b563b9aa6c5cff4dba316
Tony <12673334+Tonyx97@users.noreply.github.com>
2025-07-03 19:58:35 +0200

    Update main.lua

--- freeroam/shared/main.lua
+++ freeroam/shared/main.lua
@@ -3,5 +3,9 @@ Event.Add("PlayerKilled", function(player)
 end)
 
 Event.Add("PlayerDamage", function(player, damager, loss, hitBone, weaponHash, position)
-    print(damager:GetNetId() .. " damaged player " .. player:GetNetId() .. " with " .. loss .. " hp using weapon " .. weaponHash .. " at " .. tostring(position) .. " (bone: " .. hitBone .. ")")
+    if damager then
+        print(damager:GetNetId() .. " damaged player " .. player:GetNetId() .. " with " .. loss .. " hp using weapon " .. weaponHash .. " at " .. tostring(position) .. " (bone: " .. hitBone .. ")")
+    else
+        print("player damaged " .. player:GetNetId() .. " with " .. loss .. " hp using weapon " .. weaponHash .. " at " .. tostring(position) .. " (bone: " .. hitBone .. ")")
+    end
 end)
