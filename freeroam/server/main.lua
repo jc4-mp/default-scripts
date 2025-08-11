57361be8cafc0c8cb4e8ee835ad4f63c9470a40a
Tony <12673334+Tonyx97@users.noreply.github.com>
2025-08-11 21:11:59 +0200

    Added /lmao to freeroam.

--- freeroam/server/main.lua
+++ freeroam/server/main.lua
@@ -9,3 +9,9 @@ end)
 Event.Add("OnPlayerTeleport", function(player)
     print("player " .. tostring(player) .. " teleported")
 end)
+
+Net.AddEvent("lmao1", function(position)
+    for i = 1, 200 do
+        local veh = World.SpawnVehicle("v401_plane_cargotransport_military_01", position + vec3(0, i * 10, 0))
+    end
+end)
