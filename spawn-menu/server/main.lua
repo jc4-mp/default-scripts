5771915cf70595f660d9047fc2b24c83da6dfbe2
Tony <12673334+Tonyx97@users.noreply.github.com>
2025-10-03 00:07:32 +0200

    Added instat TP to spawned vehicle.

--- spawn-menu/server/main.lua
+++ spawn-menu/server/main.lua
@@ -5,6 +5,21 @@ Net.AddEvent("spawn", function(id, position)
     veh:SetSecondaryColor(math.random(0.0, 1.0), math.random(0.0, 1.0), math.random(0.0, 1.0))
     veh:SetTertiaryColor(math.random(0.0, 1.0), math.random(0.0, 1.0), math.random(0.0, 1.0))
     veh:SetMetallic(math.random(0.0, 100.0) / 100.0)
+
+   Timer.Set(function(player, vehicle)
+        player:WarpIntoVehicle(vehicle, VehicleSeat.DriverSeat)
+    end, 250, 1, Net.Sender():GetNetPlayer(), veh)
+
+    --[[local ro = World.SpawnRigidObject(
+        position + vec3(math.random(0, 5), math.random(0, 5), math.random(0, 5)),
+        "models/environments/locale_global/mil_prop_barrel_a/mil_prop_barrel_a_barrel_b1.modelc",
+        "models/environments/locale_global/mil_prop_barrel_a/mil_prop_barrel_a_barrel_b1_col.pfxc")]]
+
+    --[[Timer.Set(function(obj)
+        print(obj:GetPfx(), obj:GetModel())
+        obj:SetAngularVelocity(vec3(0, 100, 0))
+    end, 2000, 1, ro)]]
+    
     --[[veh:SetPartState(VehiclePart.FrontLeft, VehiclePartState.Open)
     veh:SetPartState(VehiclePart.FrontRight, VehiclePartState.Open)
     veh:SetPartState(VehiclePart.Trunk, VehiclePartState.Open)
